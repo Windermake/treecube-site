121c20567e34aee8eb0dc4a394d67aac34b7ee4e
Windermake <gogfof471@gmail.com>
2025-12-04 19:26:24 +0700

    1

---
+++ public/js/players-data.js
@@ -0,0 +1,190 @@
+const players = [
+    {
+        nick: 'endiekey',
+        real: 'Эндикей',
+        season: '2',
+        about: 'Эндикто? Эндиштерн',
+        role: 'Участник',
+        twitch: 'https://www.twitch.tv.endiekey__',
+        youtube: '',
+        videos: [
+            'https://youtu.be/S8wQ-PGn2OM?si=PFuAz6mCZ5ghIS-6',
+            'https://youtu.be/TFM3VIFzrAM?si=BxBXXMBFgz7dBNxZ',
+            'https://youtu.be/DnRmZwNbgzk?si=M3s3bbX3t-UsY4pr'
+        ],
+        merits: 'Самый активный игрок тк2',
+        youtubeChannel: ''
+    },
+    {
+        nick: 'Кукуруку',
+        real: 'Кукуруку',
+        season: '1 сезона / TreeCube origins',
+        about: 'Трипи тропа дайте дропа',
+        role: 'Участник',
+        twitch: 'Смотрите трансляции пользователя kykuryka на Twitch вместе со мной! kykuryka',
+        youtube: '',
+        videos: [
+            'https://youtu.be/MH4dHnZDE6s?si=1ucZwuc0rVn2OSFH',
+            'https://youtu.be/IYdBvyAjvKk?si=8wsbhWRmtjTTyn8k',
+            'https://youtu.be/Y5TtstXYItw?si=cueeAiRSha98TAtV',
+            'https://youtu.be/kha_V7TtaVY?si=KvLK0GrvdGziK913',
+            'https://youtu.be/SgoKkm_V2SI?si=8VPfz-CiehlIfx2J',
+            'https://youtu.be/82vR08M8Xbw?si=o0LPAimMC4x7sNd6'
+        ],
+        merits: `1. Второй призедент федералов
+2. Обладатель самого не просматримаего видео на тк 1
+3. Получил второе место по областной олимпиаде по географии
+4. Сдал ЕНТ
+5. Тот самый забытый человек на наборе ТКА
+6. Умею на велосипеде без рук ездить
+7. Наиграл 1000 часов в КС
+8. Соскуфился к 14 годам
+9. Делал нарезки по те до того как это стало мэйнстримом
+10. Был замом главы гильдии воров`,
+        youtubeChannel: ''
+    },
+    {
+        nick: 'Kroster_',
+        real: 'Кростер',
+        season: '2 сезон',
+        about: '',
+        role: 'Участник',
+        twitch: '',
+        youtube: '',
+        videos: [
+            'https://youtu.be/Jz_3HviibCo',
+            'https://youtu.be/lQCwL7xBirY',
+            'https://youtu.be/Qd0iglQ65FM',
+            'https://youtu.be/Jm8ocPFxMsA'
+        ],
+        merits: 'Основал официальное творческое сообщество по Трикубу - ДТК (Дурачки Трикуба)',
+        youtubeChannel: ''
+    },
+    {
+        nick: 'ILIADOD',
+        real: 'Илиадод',
+        season: 'С 2 сезона',
+        about: 'Ну это, короче, там, да.',
+        role: 'Участник',
+        twitch: '',
+        youtube: '',
+        videos: [],
+        merits: 'Создатель величайшего архитектурного произведения искусства под названием "банка FANTA"',
+        youtubeChannel: 'https://youtube.com/@iliadodd?si=YwwO8FKQzvHZ7NRf'
+    },
+    {
+        nick: 'megush',
+        real: 'Мегуш',
+        season: 'Со второго',
+        about: 'Я видео делаю всякие, приколы разные, шутить не умею, пытаюсь вывозить харизмой',
+        role: 'Участник',
+        twitch: 'https://m.twitch.tv/megushevskiy',
+        youtube: '',
+        videos: [
+            'https://youtu.be/ZLg9YuQ-GrI',
+            'https://youtu.be/V_flgAA-lp0'
+        ],
+        merits: `Видео набрало 60к+ просмотров
+Стал частью Трикуба
+Единственный кто украл куклу-вуду
+Построил 2 бесполезных постройки!!!`,
+        youtubeChannel: ''
+    },
+    {
+        nick: 'KNOX Play',
+        real: 'Кнокс',
+        season: 'С Трикуб Академии',
+        about: 'В Майнкрафте 11 лет, а на Ютубе 7.. а вы чего добились?',
+        role: 'Участник',
+        twitch: 'https://www.twitch.tv/knox_pl1y',
+        youtube: '',
+        videos: [
+            'https://www.youtube.com/watch?v=JJd5jWrOWp8',
+            'https://www.youtube.com/live/ogM6j8-oWNA',
+            'https://www.youtube.com/live/ogM6j8-oWNA',
+            'https://youtu.be/0xb1Xkr4QUM',
+            'https://youtu.be/gww5zQysUrQ'
+        ],
+        merits: 'Амбассадор Лора, ЛУЧШИЙ друг Денфв 1000%, участник АртНоксов',
+        youtubeChannel: ''
+    },
+    {
+        nick: '@kilka_v_hJlebe',
+        real: 'Килька',
+        season: '1',
+        about: 'Отрицательный результат тоже результат',
+        role: 'Участник',
+        twitch: 'https://www.twitch.tv/kilka_v_hjiebe',
+        youtube: '',
+        videos: ['https://www.youtube.com/playlist?list=PLLcUHd67x0-lE3civmixU_YFHhkCe3fEU'],
+        merits: 'Я не отчислился с шараги. Худший из худших.',
+        youtubeChannel: ''
+    },
+    {
+        nick: 'PernatyiRand0m',
+        real: 'Ранд0м',
+        season: 'С нулевого',
+        about: 'еее',
+        role: 'Участник',
+        twitch: '',
+        youtube: '',
+        videos: [
+            'https://youtu.be/aaPoyvXS2eA?si=520fki5kU_8yZDaP',
+            'https://youtu.be/qZ_yIhM3_OY?si=hg6l2BS2FfdOCMIm'
+        ],
+        merits: '',
+        youtubeChannel: 'https://youtube.com/@musterand0mgmd?si=8_dN83uBdG7XWDQT'
+    },
+    {
+        nick: 'ArtMine_',
+        real: 'Арт',
+        season: 'Трикуб Академия',
+        about: '«Покуда Колизей неколебим, Великий Рим стоит неколебимо. Но рухни Колизей – и рухнет Рим, И рухнет мир, когда не станет Рима».',
+        role: 'Участник',
+        twitch: 'https://www.twitch.tv/art_mine',
+        youtube: '',
+        videos: [
+            'https://youtu.be/hD_BRDfBCYo?si=i6UFdHA653f2AhaW',
+            'https://youtu.be/hWtjyC6he9s?si=BtFAR3owx7eO-3sj',
+            'https://youtu.be/Gg5TdFAEzV0?si=OFapMM_rznp8BEV-'
+        ],
+        merits: 'Прав абсолютно всегда',
+        youtubeChannel: 'https://www.youtube.com/channel/UCacTzKX5KmCSK4aSiRkd7vA'
+    },
+    {
+        nick: 'Egorik_Pantaloni',
+        real: 'Панталон',
+        season: '2',
+        about: 'Арбуз, красный, зелёный, вкусный (Тут могла быть ваша реклама)',
+        role: 'Участник',
+        twitch: '',
+        youtube: '',
+        videos: [
+            'https://www.youtube.com/watch?v=bhZsSy_xUOs',
+            'https://www.youtube.com/watch?v=QBbiai3i200'
+        ],
+        merits: 'Тот самый абориген',
+        youtubeChannel: 'https://www.youtube.com/@VazniePantaloni'
+    },
+    {
+        nick: 'MrSteak336',
+        real: 'Стейк',
+        season: '1',
+        about: 'бутовв придёт',
+        role: 'Участник',
+        twitch: 'https://www.twitch.tv/mrst3ak336',
+        youtube: '',
+        videos: [
+            'https://www.youtube.com/watch?v=vsO1i88FEME',
+            'https://www.youtube.com/watch?v=daz-Xeedxx4&t=1s',
+            'https://www.youtube.com/watch?v=xza0mmeWvaU&t=1s',
+            'https://www.youtube.com/watch?v=g4A1BfQWIR4',
+            'https://www.youtube.com/watch?v=dzcL9KNpQ_Y&t=3s',
+            'https://www.youtube.com/watch?v=y2ypdYkXkGU&t=129s',
+            'https://www.youtube.com/watch?v=dFz6Tr31qmY',
+            'https://www.youtube.com/watch?v=GYSxdyzyeYg'
+        ],
+        merits: 'самый молодой игрок тк, забрал статуэтку за лучшую базу сезона на прошлом форевере',
+        youtubeChannel: 'https://www.youtube.com/@mrsteak336/featured'
+    }
+];
